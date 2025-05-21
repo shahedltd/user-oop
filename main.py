a77b0809bec2acfbdbfc2c50ae73434071fd8c78
Shahed Rahman <153431638+shahedltd@users.noreply.github.com>
2025-05-22 01:27:40 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+class User:
+    user_count = 0
+
+    def __init__(self, username, email, password):
+        self.username = username  
+        self.email = email
+        self.__password = password
+        User.user_count += 1
+    
+    @property
+    def password(self):
+        return self.__password  
+    
+    @password.setter
+    def password(self, new_password):
+        self.__password = new_password
+
+    def account_check(self, pincode):
+        if self.__password == pincode:
+            return True
+        return False
+    
+    def __str__(self):
+        return f"Hello, I have an account and my account's username is: {self.username}.\nAnd here is my email: {self.email}"
+
+
+# Test the code
+user1 = User("shahedrahman", "shahed@xyz.com", 1127)
+
+
+
+if user1.account_check(1127):  
+    print("Login successful!")
+    print(user1)
+else:
+    print("Invalid password, try again.")
